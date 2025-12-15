e9f81deba71cbc25638df1cf28e462449e93ac0e
pawan_prudhvi <pawanprudhvi88@gmail.com>
2025-12-15 16:45:27 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-200'>
+        <h1 className='text-2xl font-bold text-red-800'>pathflow yaehsrdkfghjg</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
