1bd227ef7431065f4b1c47e7e55a1110a0610e6e
adhithan <rm.adhithan2002@gmail.com>
2025-12-15 17:52:58 +0530

    do it

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <div className='bg-red-200'>
-        <h1 className='text-2xl font-bold text-red-800'>pathflow yaehsrdkfghjg</h1>
+        <h1 className='text-2xl font-bold text-red-800'>pathflow</h1>
       </div>
     </>
   )
